27f9c00fa0fa4266ec07e77e997fda99f1172931
Hernandes-PC <hernandeshass910@gmail.com>
2023-09-20 11:03:26 -0300

    Corrigido

--- Desafio.java
+++ Desafio.java
@@ -1,49 +1,51 @@
 import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
 
-public class Desafio{
-    public static void main(String[] args){
-        String[] nomes = new String [10];
-        Double[][] notas = new Double [10][3];
-        Double[][] media = new Double [10][3];
-        Double num=0.0;
+public class Desafio {
+    public static void main(String[] args) {
+        String[] nomes = new String[10];
+        Double[][] notas = new Double[10][3];
+        Double[][] media = new Double[10][1];
+        Double num = 0.0, decimal=0.0;
         String result;
         int l, c;
 
-        for(l=0;l<10;l++){
-            nomes[l] = JOptionPane.showInputDialog("Digite o nome do " +(l+1) +"° aluno");
-        }
+        for (l = 0; l < 3; l++) {
+            nomes[l] = JOptionPane.showInputDialog("Digite o nome do " + (l + 1) + "° aluno");
 
-        for(l=0;l<10;l++){
-            for(c=0;c<3;c++){
-                notas[l][c] = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota do " +(c+1) +"° Bimestre do aluno(a) " +nomes[l]));
+            for (c = 0; c < 3; c++) {
+                notas[l][c] = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota do " + (c + 1) + "° Bimestre do aluno(a) " + nomes[l]));
+                while(notas[l][c] > 10 || notas[l][c] <= 0) {
+                	JOptionPane.showMessageDialog(null, "Digite uma nota válida");
+                	notas[l][c] = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota do " + (c + 1) + "° Bimestre do aluno(a) " + nomes[l]));
+                }
             }
         }
-        
-        for(l=0;l<10;l++){
-            for(c=0;c<3;c++){
+
+        for (l = 0; l < 3; l++) {
+            for (c = 0; c < 3; c++) {
                 num += notas[l][c];
             }
-            for(c=0;c<1;c++){
-                media[l][c]=num;
-            }
-            num=0.0;
+            media[l][0] = num / 3;
+            num = 0.0;
         }
-        
-        System.out.print("***** Média ***** \n");
 
-        for(l=0;l<10;l++){
-            for(c=0;c<1;c++){
-                if((media[l][c]/3)<7){
-                    result=(nomes[l] +"\t" +notas[l][c] +"\t" +notas[1][1] +"\t" +notas[l][c] +"\t" +((media[l][c])/3)+"*");
-                    System.out.print(result);
-                }
-                else{
-                    result=(nomes[l] +"\t" +notas[l][c] +"\t" +notas[l][c] +"\t" +notas[l][c] +"\t" +((media[l][c])/3));
-                    System.out.print(result);
-                }
+        System.out.println("\t***** Média *****");
+        System.out.println("Aluno\t\t\tNotas\t\tMédia");
+        
+        DecimalFormat df = new DecimalFormat("0.00");
+        
+        for (l = 0; l < 3; l++) {
+            result = nomes[l] + "\t\t";
+            for (c = 0; c < 3; c++) {
+                result += notas[l][c] + "\t";
             }
-            System.out.print("\n");
+            decimal = media[l][0];
+            result += df.format(decimal);
+            if (media[l][0] < 7) {
+                result += "*";
+            }
+            System.out.println(result);
         }
     }
 }
-
